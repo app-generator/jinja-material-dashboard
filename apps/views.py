a1d9e92279ac39e830ddb9278c6149efed6c5acb
imran-helios <imran.mridha@heliosinsider.com>
2023-02-18 15:25:35 +0600

    Render all pages

--- apps/views.py
+++ apps/views.py
@@ -15,9 +15,66 @@ from apps import app
 @app.route('/')
 def index():
   try:
-    return render_template( 'pages/index.html', segment='analytics', parent='dashboard')
+    return render_template( 'pages/index.html', segment='dashboard', parent='pages')
   except TemplateNotFound:
     return render_template('pages/index.html'), 404
+
+
+
+@app.template_filter(name = 'replace_value')
+def replace_value(value, arg):
+  return value.replace(arg, ' ').title()
+
+# Pages
+
+@app.route('/pages/tables/')
+def pages_tables():
+  return render_template('pages/tables.html', segment='tables', parent='pages')
+
+@app.route('/pages/billing/')
+def pages_billing():
+  return render_template('pages/billing.html', segment='billing', parent='pages')
+
+@app.route('/pages/virtual-reality/')
+def pages_virtual_reality():
+  return render_template('pages/virtual-reality.html', segment='virtual_reality', parent='pages')
+
+@app.route('/pages/rtl/')
+def pages_rtl():
+  return render_template('pages/rtl.html', segment='rtl', parent='pages')
+
+@app.route('/pages/notifications/')
+def pages_notifications():
+  return render_template('pages/notifications.html', segment='notifications', parent='pages')
+
+@app.route('/pages/profile/')
+def pages_profile():
+  return render_template('pages/profile.html', segment='profile', parent='pages')
+
+@app.route('/accounts/login/')
+def accounts_login():
+  return render_template('accounts/login.html', segment='login', parent='accounts')
+
+@app.route('/accounts/register/')
+def accounts_register():
+  return render_template('accounts/register.html', segment='register', parent='accounts')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # Dashboard
